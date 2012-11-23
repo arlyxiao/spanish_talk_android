4ac35f84b10ec0d18493f394897d5be4acb7bc5d
mykingla <kingla_pei@163.com>
2012-11-23 11:32:42 +0800

    Initial register layout page

--- gen/com/example/spanishtalk/R.java
+++ gen/com/example/spanishtalk/R.java
@@ -11,25 +11,54 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int bg_button_head_bar=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button1=0x7f070002;
+        public static final int editText1=0x7f070001;
+        public static final int editText2=0x7f070000;
+        public static final int menu_settings=0x7f070008;
+        public static final int register_button=0x7f070007;
+        public static final int register_edittext_affirm_password=0x7f070006;
+        public static final int register_edittext_email=0x7f070003;
+        public static final int register_edittext_name=0x7f070004;
+        public static final int register_edittext_password=0x7f070005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_register=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_login=0x7f060000;
+        public static final int activity_main=0x7f060001;
+        public static final int activity_register=0x7f060002;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int button_go_back=0x7f050006;
+        public static final int click_register_button=0x7f05000b;
+        public static final int confirm_password=0x7f05000a;
+        public static final int email=0x7f050008;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int password=0x7f050009;
+        public static final int title_activity_login=0x7f050004;
+        public static final int title_activity_main=0x7f050003;
+        public static final int title_activity_register=0x7f050005;
+        public static final int username=0x7f050007;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f040004;
+        public static final int body=0x7f040000;
+        public static final int head_bar=0x7f040005;
+        public static final int head_bar_button=0x7f040006;
+        public static final int head_bar_button_go_back_register=0x7f040007;
+        public static final int list_item=0x7f040003;
+        public static final int login_layout_button=0x7f040008;
+        public static final int no_padding_body=0x7f040002;
+        public static final int padding_10_body=0x7f040001;
     }
 }
